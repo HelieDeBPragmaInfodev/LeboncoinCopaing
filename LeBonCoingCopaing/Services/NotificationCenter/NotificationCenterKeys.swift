17b866be949b1b3d59de5c3fad0073775eabf31e
Helie <h4bdev@gmail.com>
2022-08-08 20:19:05 +0200

    Commit file CenterKeys

---
+++ LeBonCoingCopaing/Services/NotificationCenter/NotificationCenterKeys.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationCenterKeys.swift
+//  LeBonCoingCopaing
+//
+//  Created by Hélie de Bernis on 08/08/2022.
+//
+
+import Foundation
+
+class NotificationCenterKeys {
+    static let listOfListingReceivedSuccessfully = "UserDidReceivedListOfListingSuccessfully"
+    static let userDidReceivedSmallThumbnail = "UserDidReceivedSmallThumbnail"
+    static let userDidReceivedFullScreenThumbnail = "userDidReceivedFullScreenThumbnail"
+}
